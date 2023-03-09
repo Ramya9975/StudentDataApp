d38d49170d9663899897cb75fb0ce4c7867a7403
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-03-09 14:43:24 +0530

    Add project files.

---
+++ StudentDataApp/Employeedetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentDataApp
+{
+    public partial class Employeedetails : Form
+    {
+        public Employeedetails()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Server = NLTI159\\SQLEXPRESS; Database = Employment; integrated Security = true");
+            SqlCommand command = new SqlCommand($"SELECT s.FirstName,s.LastName,s.Department,e.Company,e.Salary\r\nFROM Student s\r\nJOIN Information e\r\nON S.StudentId = e.ID order by e.Salary DESC", con);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Server = NLTI159\\SQLEXPRESS; Database = Employment; integrated Security = true");
+            SqlCommand command = new SqlCommand($"SELECT s.FirstName,s.LastName,s.Department,e.Company,e.Salary,e.designation\r\nFROM Student s\r\nJOIN Information e\r\nON S.StudentId = e.ID Group by e.designation,s.FirstName,s.LastName,s.Department,e.Company,e.Salary", con);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+
+
+        }
+    }
+}
---
+++ StudentDataApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentDataApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int studentId = Convert.ToInt32(comboBox1.GetItemText(comboBox1.SelectedItem));
+            SqlConnection con = new SqlConnection("Server = NLTI159\\SQLEXPRESS; Database = Employment; integrated Security = true");
+            SqlCommand command = new SqlCommand($"SELECT *\r\nFROM Student\r\nJOIN Information\r\nON Student.StudentId = Information.ID WHERE StudentId = {studentId}", con);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable= new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridView1.DataSource= dataTable;
+               
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Employeedetails form = new Employeedetails();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
